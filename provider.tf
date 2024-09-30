9aaf45ab85e16132f0dcbcfb02f54f5ddf2a3475
pmadhureddy <pmadhureddy1510@gmail.com>
2024-09-30 21:11:54 +0530

    Using VPC module to create vpc and subnets altogether

---
+++ provider.tf
@@ -0,0 +1,22 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+
+  backend "s3" {
+    bucket         = "madhu-locking-state"
+    key            = "expense-vpc"
+    region         = "us-east-1"
+    dynamodb_table = "terraformlocking"
+  }
+
+
+}
+
+# Configure the AWS Provider
+provider "aws" {
+  region = "us-east-1"
+}
